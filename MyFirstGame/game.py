9896b1133417cda80ecaaa0dcb73b7af59126a8d
roaco <roaco91@gmail.com>
2023-03-12 23:58:31 -0500

    Se actualiza proyecto mejorando la funcionalidad y habilitando que pierde

--- MyFirstGame/game.py
+++ MyFirstGame/game.py
@@ -1,150 +1,154 @@
 import pygame
 import random
-
+ 
 # Inicializar Pygame
 pygame.init()
-
-# Colores
-NEGRO = (0, 0, 0)
-BLANCO = (255, 255, 255)
-ROJO = (255, 0, 0)
-AZUL = (0, 0, 255)
-VERDE = (0, 255, 0)
-
-# Tamaño de la ventana
-ANCHO = 800
-ALTO = 600
-
-# Crear la ventana
-pantalla = pygame.display.set_mode((ANCHO, ALTO))
+ 
+# Establecer el tamaño de la pantalla
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Breakout")
-
-# Clase para la pelota
-class Pelota(pygame.sprite.Sprite):
+ 
+# Definir los colores que se utilizarán en el juego
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+ 
+# Definir la paleta del jugador
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([80, 10])
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH // 2 - 40
+        self.rect.y = SCREEN_HEIGHT - 50
+        self.speed = 0
+ 
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+ 
+    def move_left(self):
+        self.speed = -10 # Definimos velocidad de la barra
+ 
+    def move_right(self):
+        self.speed = 10 # Definimos velocidad de la barra
+ 
+    def stop(self):
+        self.speed = 0
+ 
+# Definir el bloque
+class Block(pygame.sprite.Sprite):
+    def __init__(self, color):
+        super().__init__()
+        self.image = pygame.Surface([48, 20])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+ 
+# Crear los bloques
+all_sprites_list = pygame.sprite.Group()
+block_list = pygame.sprite.Group()
+for i in range(16): #Se establece la cantidad de filas
+    for j in range(5): # Se establece la cantidad de columnas
+        block = Block(RED)
+        block.rect.x = i * (block.rect.width + 2) + 10
+        block.rect.y = j * (block.rect.height + 2) + 10
+        block_list.add(block)
+        all_sprites_list.add(block)
+ 
+# Crear la paleta del jugador
+player_paddle = Paddle()
+all_sprites_list.add(player_paddle)
+ 
+# Definir la pelota
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.Surface([10, 10])
-        self.image.fill(BLANCO)
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
-        self.velocidad = [random.randint(4,8), random.randint(-8,8)]
-
+        self.rect.x = SCREEN_WIDTH // 2 - 5
+        self.rect.y = SCREEN_HEIGHT - 70
+        self.speed_x = 4 #DEFINIMOS VELOCIDAD DE LA PELOTA
+        self.speed_y = -4 #DEFINIMOS VELOCIDAD DE LA PELOTA
+ 
     def update(self):
-        self.rect.x += self.velocidad[0]
-        self.rect.y += self.velocidad[1]
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
+            self.speed_x = -self.speed_x
+        elif self.rect.top < 0:
+            self.speed_y = -self.speed_y
+        elif self.rect.bottom > SCREEN_HEIGHT:
+            pygame.quit()
+            quit()
+ 
+        # Colisión de la pelota con la paleta
+        if pygame.sprite.collide_rect(self, player_paddle):
+            self.speed_y = -self.speed_y
+            self.speed_x +=  0.001 #DEFINIMOS AUMENTO EN LA VELOCIDAD DE LA PELOTA
+            self.speed_y += - 0.001 #DEFINIMOS AUMENTO EN LA VELOCIDAD DE LA PELOTA
+            
+        # Colisión de la pelota con los bloques
+        blocks_hit_list = pygame.sprite.spritecollide(self, block_list, True)
+        for block in blocks_hit_list:
+            global score
+            score += 1
+        if len(blocks_hit_list) > 0:
+            self.speed_y = -self.speed_y
+ 
+# Crear la pelota
+ball = Ball()
+all_sprites_list.add(ball)
 
-        if self.rect.x > ANCHO - 10 or self.rect.x < 0:
-            self.velocidad[0] = -self.velocidad[0]
-        if self.rect.y > ALTO - 10 or self.rect.y < 0:
-            self.velocidad[1] = -self.velocidad[1]
 
-# Clase para la pala
-class Pala(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface([75, 10])
-        self.image.fill(ROJO)
-        self.rect = self.image.get_rect()
-        self.rect.x = ANCHO // 2 - 37
-        self.rect.y = ALTO - 50
-        self.velocidad = 0
-
-    def update(self):
-        self.rect.x += self.velocidad
-        if self.rect.x < 0:
-            self.rect.x = 0
-        elif self.rect.x > ANCHO - 75:
-            self.rect.x = ANCHO - 75
-
-# Clase para los bloques
-class Bloque(pygame.sprite.Sprite):
-    def __init__(self, color, x, y):
-        super().__init__()
-        self.image = pygame.Surface([75, 20])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-# Crear los bloques
-bloques = pygame.sprite.Group()
-for fila in range(5):
-    for columna in range(10):
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        bloque = Bloque(color, columna * 75, fila * 20 + 50)
-        bloques.add(bloque)
-
-# Crear los sprites
-pelota = Pelota()
-pala = Pala()
-todos_los_sprites = pygame.sprite.Group()
-todos_los_sprites.add(pelota)
-todos_los_sprites.add(pala)
-todos_los_sprites.add(bloques)
-
-# Fuente de texto
-fuente = pygame.font.Font(None, 36)
-
-# Variables para el juego
-vidas = 3
-puntos = 0
-
+# Definir el marcador y variables del juego
+score = 0
+font = pygame.font.Font(None, 36)
+ 
 # Bucle principal del juego
-hecho = False
-reloj = pygame.time.Clock()
-
-while not hecho:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            hecho = True
-        elif evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_LEFT:
-                pala.velocidad = -8
-            elif evento.key == pygame.K_RIGHT:
-                pala.velocidad = 8
-        elif evento.type == pygame.KEYUP:
-            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
-                pala.velocidad = 0
-
-    # Actualizar los sprites
-    todos_los_sprites.update()
-
-    # Colisiones de la pelota con la pala
-    if pygame.sprite.collide_rect(pelota, pala):
-        pelota.velocidad[1] = -pelota.velocidad[+1]
-
-    # Colisiones de la pelota con los bloques
-    bloques_rotos = pygame.sprite.spritecollide(pelota, bloques, True)
-    if bloques_rotos:
-        pelota.velocidad[1] = -pelota.velocidad[1]
-        puntos += len(bloques_rotos)
-        if len(bloques) == 0:
-            hecho = True
-
-    # Si la pelota se va por debajo de la pantalla
-    if pelota.rect.y > ALTO:
-        vidas -= 1
-        if vidas == 0:
-            hecho = True
-        else:
-            pelota.rect.x = ANCHO // 2 - 5
-            pelota.rect.y = ALTO // 2 - 5
-            pelota.velocidad = [random.randint(4,8), random.randint(-8,8)]
-
-    # Dibujar la pantalla
-    pantalla.fill(NEGRO)
-    todos_los_sprites.draw(pantalla)
-
-    # Dibujar texto en la pantalla
-    texto_vidas = fuente.render("Vidas: {}".format(vidas), True, BLANCO)
-    pantalla.blit(texto_vidas, (10, 10))
-    texto_puntos = fuente.render("Puntos: {}".format(puntos), True, BLANCO)
-    pantalla.blit(texto_puntos, (ANCHO - 150, 10))
-
-    # Actualizar la pantalla
+game_over = False
+clock = pygame.time.Clock()
+ 
+while not game_over:
+    # Procesamiento de eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_paddle.move_left()
+            elif event.key == pygame.K_RIGHT:
+                player_paddle.move_right()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and player_paddle.speed < 0:
+                player_paddle.stop()
+            elif event.key == pygame.K_RIGHT and player_paddle.speed > 0:
+                player_paddle.stop()
+ 
+    # Actualizar objetos del juego
+    all_sprites_list.update()
+ 
+    # Verificar si la pelota ha golpeado la parte inferior de la pantalla
+    if ball.rect.bottom > SCREEN_HEIGHT:
+        game_over = True
+ 
+    # Dibujar objetos del juego
+    screen.fill(BLACK)
+    all_sprites_list.draw(screen)
+    score_text = font.render("Score: {}".format(score), True, WHITE)
+    screen.blit(score_text, [10, 10])
     pygame.display.flip()
-
-    # Controlar el número de frames por segundo
-    reloj.tick(60)
-
-# Salir del juego
+ 
+    # Establecer el límite de fotogramas
+    clock.tick(60)
+ 
+# Cerrar Pygame
 pygame.quit()
